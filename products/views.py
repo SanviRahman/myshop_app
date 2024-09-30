61299926570220a471e344fd7ddd455befbfde68
SanviRahman <sheikh15-3700@diu.edu.bd>
2024-09-30 12:30:29 +0600

    first commit

--- products/views.py
+++ products/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from .models import Product
+from django.db.models import Avg,Sum,Count
+from .models import Product,Category
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
+    cateries = Category.objects.all()
+    products = Product.objects.all().order_by("price")
+
+
+    #Agrrogration
+    price_avg= products.aggregate(Avg('price'))["price__avg"]
+    price_sum= products.aggregate(Sum('price'))["price__sum"]
+
+
+    #Anotate
+    cateries = Category.objects.annotate(product_count=Count('product'))
+
 
     context = {
         'products': products,
+        'categories': cateries,
+        'price_avg': price_avg,
+        'price_sum': price_sum,
     }
 
     return render(request, 'products/index.html',context)
